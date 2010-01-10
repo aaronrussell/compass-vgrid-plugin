a33fbad0c966248ceeb982e8401c63578ccad21f
Aaron Russell <aaron@gc4.co.uk>
2010-01-10 15:56:10 +0000

    Fixed a couple of syntax bugs and updated both fixed and fluid templates to bring them in line with each other

--- compass-vgrid-plugin.gemspec
+++ compass-vgrid-plugin.gemspec
@@ -23,6 +23,7 @@ Gem::Specification.new do |s|
      "README.md",
      "Rakefile",
      "VERSION",
+     "compass-vgrid-plugin.gemspec",
      "lib/vgrid.rb",
      "lib/vgrid/compass_plugin.rb",
      "sass/vgrid/_elastic.sass",
--- lib/vgrid/compass_plugin.rb
+++ lib/vgrid/compass_plugin.rb
@@ -1,6 +1,6 @@
 ["fixed", "fluid", "elastic"].each do |framework|
   options = {
-    :stylesheets_directory => File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'sass'))
+    :stylesheets_directory => File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'sass')),
     :templates_directory => File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'templates', framework))
   }
   Compass::Frameworks.register(framework, options)
