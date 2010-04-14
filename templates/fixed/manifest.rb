4779a2eef06e72f57eeadcce7caf66872f0064b1
Chris Eppstein <chris@eppsteins.net>
2010-04-13 20:12:16 -0700

    Some bells and whistles

--- templates/fixed/manifest.rb
+++ templates/fixed/manifest.rb
@@ -1,3 +1,16 @@
 stylesheet  "screen.scss", :media => "screen, projection"
 file        "demo.html"
 
+description "The fixed width setup for vgrid."
+
+help %Q{
+Installs stylesheets that demonstrate how to access the fixed-width vgrid libraries.
+
+For more information about The Variable Grid System by SprySoft:
+
+  http://www.spry-soft.com/grids/
+}
+
+welcome_message %Q{
+You are now configured to use the fixed-width version of vgrid!
+}
--- templates/fluid/manifest.rb
+++ templates/fluid/manifest.rb
@@ -1,2 +1,16 @@
 stylesheet  "screen.scss", :media => "screen, projection"
 file        "demo.html"
+
+description "The fluid setup for vgrid."
+
+help %Q{
+Installs stylesheets that demonstrate how to access the fluid vgrid libraries.
+
+For more information about The Variable Grid System by SprySoft:
+
+  http://www.spry-soft.com/grids/
+}
+
+welcome_message %Q{
+You are now configured to use the fluid version of vgrid!
+}
